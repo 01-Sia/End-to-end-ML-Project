b739a302a79fffad6638d6a536f600c12f608aef
01-Sia <117347998+01-Sia@users.noreply.github.com>
2025-03-04 14:04:34 +0530

    Logging,exception added

---
+++ src/logger.py
@@ -0,0 +1,31 @@
+import logging
+import os 
+from datetime import datetime 
+'''  This logging file is necessary :
+Imagine running a restaurant without keeping track of orders or feedback — it would be chaos! 
+The logger ensures every significant event is recorded. '''
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok = True)
+
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+
+    filename =LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+
+
+
+)
+
+
+
+
+if __name__=="__main__":
+
+    logging.info("Logging file test")
+    
